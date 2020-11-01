b3bb469f61f10be9ceed8271f4a516449d54ad14
lgrayes <lgrayes@gmail.com>
2020-11-01 13:36:09 -0600

    upload manager table

---
+++ EmployeeSQL/managerTable.sql
@@ -0,0 +1,13 @@
+-- List the manager of each department with the following information: 
+-- department number, 
+-- department name, 
+-- the manager's employee number, 
+-- last name, 
+-- first name.
+
+CREATE TABLE departmentshk (
+dept_no varchar,
+dept_name varchar
+);
+
+select * from departmentshk
--- EmployeeSQL/uploadEmpTable.sql
+++ EmployeeSQL/uploadEmpTable.sql
@@ -44,4 +44,17 @@ employeesHK.sex
 FROM employeeshk
 WHERE first_name = 'Hercules' and last_name LIKE 'B%'
 
+-- List the manager of each department with the following information: 
+-- department number, 
+-- department name, 
+-- the manager's employee number, 
+-- last name, 
+-- first name.
 
+
+CREATE TABLE departmentshk (
+dept_no varchar,
+dept_name varchar
+);
+
+select * from departmentshk
