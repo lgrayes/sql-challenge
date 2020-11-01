1ff37787890c0f3999592822c0c17c9077bbde0d
lgrayes <lgrayes@gmail.com>
2020-11-01 11:21:28 -0600

    Employee Table
    uploaded employee table to main branch

---
+++ uploadEmpTable.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS employeeshk;
+
+CREATE TABLE employeeshk (
+	emp_no varchar(255),
+	emp_title_id varchar(255),
+	birth_date varchar(255),
+	first_name varchar(255),
+	last_name varchar(255),
+	sex varchar(255),
+	hire_date date
+);
+
+SELECT * FROM employeeshk;
