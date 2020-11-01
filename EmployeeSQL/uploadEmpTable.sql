5a2851893e5bcea8cc473f7115fc2ede2041268f
lgrayes <lgrayes@gmail.com>
2020-11-01 13:14:56 -0600

    Question #2
    Question #2 answered

---
+++ EmployeeSQL/uploadEmpTable.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS employeeshk;
+DROP TABLE IF EXISTS salarieshk;
+
+CREATE TABLE employeeshk (
+	emp_no varchar(255) primary key,
+	emp_title_id varchar(255),
+	birth_date varchar(255),
+	first_name varchar(255),
+	last_name varchar(255),
+	sex varchar(255),
+	hire_date date
+);
+
+SELECT * FROM employeeshk;
+
+CREATE TABLE salarieshk (
+emp_no varchar,
+salary int,
+FOREIGN KEY (emp_no) REFERENCES employeeshk(emp_no)
+);
+
+SELECT * FROM salarieshk;
+
+SELECT employeesHK.last_name, 
+employeesHK.first_name, 
+employeesHK.sex,
+salarieshk.salary
+FROM employeesHK
+INNER JOIN salarieshk ON
+employeesHK.emp_no=salarieshk.emp_no;
+
+--  List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employeesHK.first_name,
+employeesHK.last_name, 
+employeesHK.hire_date
+FROM employeeshk
+WHERE hire_date >= '1986-01-01' and hire_date <= '1986-12-30';
--- final.sql
+++ final.sql
@@ -8,3 +8,27 @@ salarieshk.salary
 FROM employeesHK
 INNER JOIN salarieshk ON
 employeesHK.emp_no=salarieshk.emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employeesHK.first_name,
+employeesHK.last_name, 
+employeesHK.hire_date
+FROM employeeshk
+WHERE hire_date >= '1986-01-01' and hire_date <= '1986-12-30';
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
